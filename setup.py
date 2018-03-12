74af265a3f2b072241354633ad274d3650fd7e88
Sergio La Rosa <sergio.larosa89@gmail.com>
2018-03-12 16:06:15 +0100

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='splitnjoin',
-    version='0.2',
+    version='0.3',
     description='Simple module for splitting files into multiple chunks and viceversa (from chunks to original file)',
     url='https://github.com/SergioLaRosa/splitnjoin',
     author='Sergio La Rosa',

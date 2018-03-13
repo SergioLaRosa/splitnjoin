b77b1ea3b8ea86704929773cc7fb6dd11a15965b
Sergio La Rosa <sergio.larosa89@gmail.com>
2018-03-13 16:49:47 +0100

    v0.4
    MemoryError exceptions added; error output cleanup

--- splitnjoin/file_processing.py
+++ splitnjoin/file_processing.py
@@ -27,9 +27,14 @@ class FileProcessor():
                         self._chunk = self._curr_file.read(chunk_size)
                         if not self._chunk:
                             break
+                    except MemoryError as mem_err:
+                        print(
+                            "[!] Memory Error: try changing chunksize!")
+                        print("[*] Details:", mem_err)
+                        sys.exit()
                     except IOError as read_err:
-                        print("Error: unable to read file chunk(s)!")
-                        print("Details:", read_err)
+                        print("[!] Error: unable to read file chunk(s)!")
+                        print("[*] Details:", read_err)
                         sys.exit()
 
                     self._part_num += 1
@@ -39,21 +44,24 @@ class FileProcessor():
                             to_dir, (str(from_file) + '_part_%04d' %
                                      self._part_num))
                     except OSError as os_err:
-                        print("Error: unable to prepare chunk(s) path!")
-                        print("Details:", os_err)
+                        print("[!] Error: unable to prepare chunk(s) path!")
+                        print("[*] Details:", os_err)
                         sys.exit()
                     try:
                         with open(self._filename, 'wb') as self._fileobj:
                             self._fileobj.write(self._chunk)
                     except IOError as write_err:
-                        print("Error: unable to write chunk(s)!")
-                        print("Details:", write_err)
+                        print("[!] Error: unable to write chunk(s)!")
+                        print("[*] Details:", write_err)
                         sys.exit()
 
             return self._part_num
 
         except FileNotFoundError:
-            print("Error: file", _curr_file, "not found")
+            print("[!] Error: file", from_file, "not found!")
+            sys.exit()
+        except KeyboardInterrupt:
+            print("[!] Script interrupted (ctrl+C)")
             sys.exit()
 
     def _join_file(self, from_dir, to_file, readsize):
@@ -63,16 +71,16 @@ class FileProcessor():
                     self._parts = os.listdir(from_dir)
                     self._parts.sort()
                 except OSError as os_err:
-                    print("Error: unable to list chunks!")
-                    print("Details:", os_err)
+                    print("[!] Error: unable to list chunks!")
+                    print("[*] Details:", os_err)
                     sys.exit()
 
                 for self._filename in self._parts:
                     try:
                         self._filepath = os.path.join(from_dir, self._filename)
                     except OSError as os_err:
-                        print("Error: unable to preapare joined file path!")
-                        print("Details:", os_err)
+                        print("[!] Error: unable to preapare joined file path!")
+                        print("[*] Details:", os_err)
                         sys.exit()
                     try:
                         with open(self._filepath, 'rb') as self._fileobj:
@@ -82,25 +90,34 @@ class FileProcessor():
                                         int(readsize))
                                     if not self._filebytes:
                                         break
+                                except MemoryError as mem_err:
+                                    print(
+                                        "[!] Memory Error: try changing readsize!")
+                                    print("[*] Details:", mem_err)
+                                    sys.exit()
                                 except IOError as read_err:
-                                    print("Error: unable to read chunk(s)!")
-                                    print("Details:", read_err)
+                                    print("[!] Error: unable to read chunk(s)!")
+                                    print("[*] Details:", read_err)
                                     sys.exit()
                                 try:
                                     self._output.write(self._filebytes)
                                 except IOError as write_err:
-                                    print("Error: unable to write joined file!")
-                                    print("Details:", write_err)
+                                    print(
+                                        "[!] Error: unable to write joined file!")
+                                    print("[*] Details:", write_err)
                                     sys.exit()
 
                             self._fileobj.close()
 
                     except IOError as read_err:
-                        print("Error: unable to find chunk path!")
-                        print("Details:", read_err)
+                        print("[!] Error: unable to find chunk path!")
+                        print("[*] Details:", read_err)
                         sys.exit()
 
         except IOError as write_err:
-            print("Error: unable to prepare joined file!")
-            print("Details:", write_err)
+            print("[!] Error: unable to prepare joined file!")
+            print("[*] Details:", write_err)
+            sys.exit()
+        except KeyboardInterrupt:
+            print("[!] Script interrupted (ctrl+C)")
             sys.exit()

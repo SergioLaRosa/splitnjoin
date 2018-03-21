77eb2f8dbddfcf19392eda2e27fb62c13e957660
Sergio La Rosa <sergio.larosa89@gmail.com>
2018-03-21 19:52:04 +0100

    ver_0.60, "logging" module used for exceptions
    - Greatly improved exceptions/errors handling: removed "print" outputs in favor of "logging" error msgs.
    - Added "real_size" variable inside FileProcessor class: now it's calculated only once time per split/join, in order to improve performance.
    - General code clean-up

--- splitnjoin/file_processing.py
+++ splitnjoin/file_processing.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import hashlib
-
+import logging
 
 class FileProcessor:
 
@@ -14,99 +13,78 @@ class FileProcessor:
 
     def split_file(self, from_file, chunk_size, to_dir):
         self._part_num = 0
-
+        self._real_size = self._get_chunk_size(chunk_size)
         if not os.path.exists(to_dir):
             os.mkdir(to_dir)
         else:
             for fname in os.listdir(to_dir):
                 os.remove(os.path.join(to_dir, fname))
-
         try:
             with open(from_file, 'rb') as self._curr_file:
                 while True:
                     try:
                         self._chunk = self._curr_file.read(
-                            self._get_chunk_size(chunk_size))
+                            self._real_size)
                         if not self._chunk:
                             break
                     except MemoryError as mem_err:
-                        print(
-                            "[!] Memory Error: try changing chunksize!")
-                        print("[*] Details:", mem_err)
+                        logging.error(str(mem_err))
                     except IOError as read_err:
-                        print("[!] Error: unable to read file chunk(s)!")
-                        print("[*] Details:", read_err)
-
+                        logging.error(str(read_err))
                     self._part_num += 1
-
                     try:
                         self._filename = os.path.join(
                             to_dir, (str(from_file) + '_part_%04d' %
                                      self._part_num))
                     except OSError as os_err:
-                        print("[!] Error: unable to prepare chunk(s) path!")
-                        print("[*] Details:", os_err)
+                        logging.error(str(os_err))
                     try:
                         with open(self._filename, 'wb') as self._fileobj:
                             self._fileobj.write(self._chunk)
                     except IOError as write_err:
-                        print("[!] Error: unable to write chunk(s)!")
-                        print("[*] Details:", write_err)
-                        
+                        logging.error(str(write_err))
             return self._part_num
-
-        except FileNotFoundError:
-            print("[!] Error: file", from_file, "not found!")
-        except KeyboardInterrupt:
-            print("[!] Script interrupted (ctrl+C)")
+        except FileNotFoundError as notfnd_err:
+            logging.error(str(notfnd_err))
+        except Exception as generic_err:
+            logging.error(str(generic_err))
             
-    def join_file(self, from_dir, readsize, to_file):
+    def join_file(self, from_dir, chunk_size, to_file):
+        self._real_size = self._get_chunk_size(chunk_size)
         try:
             with open(to_file, 'wb') as self._output:
                 try:
                     self._parts = os.listdir(from_dir)
                     self._parts.sort()
                 except OSError as os_err:
-                    print("[!] Error: unable to list chunks!")
-                    print("[*] Details:", os_err)
-                    
+                    logging.error(str(os_err))
                 for self._filename in self._parts:
                     try:
                         self._filepath = os.path.join(from_dir, self._filename)
                     except OSError as os_err:
-                        print("[!] Error: unable to preapare joined file path!")
-                        print("[*] Details:", os_err)    
+                        logging.error(str(os_err))
                     try:
                         with open(self._filepath, 'rb') as self._fileobj:
                             while True:
                                 try:
                                     self._filebytes = self._fileobj.read(
-                                        self._get_chunk_size(readsize))
+                                        self._real_size)
                                     if not self._filebytes:
                                         break
                                 except MemoryError as mem_err:
-                                    print(
-                                        "[!] Memory Error: try changing readsize!")
-                                    print("[*] Details:", mem_err)
+                                    logging.error(str(mem_err))
                                 except IOError as read_err:
-                                    print("[!] Error: unable to read chunk(s)!")
-                                    print("[*] Details:", read_err)
+                                    logging.error(str(read_err))
                                 try:
                                     self._output.write(self._filebytes)
                                 except IOError as write_err:
-                                    print(
-                                        "[!] Error: unable to write joined file!")
-                                    print("[*] Details:", write_err)
-
+                                    logging.error(str(write_err))
                             self._fileobj.close()
-
+                    except FileNotFoundError as notfnd_err:
+                        logging.error(str(notfnd_err))
                     except IOError as read_err:
-                        print("[!] Error: unable to find chunk path!")
-                        print("[*] Details:", read_err)
-
+                        logging.error(str(read_err))
         except IOError as write_err:
-            print("[!] Error: unable to prepare joined file!")
-            print("[*] Details:", write_err)
-        except KeyboardInterrupt:
-            print("[!] Script interrupted (ctrl+C)")
-            
+            logging.error(str(write_err))
+        except Exception as generic_err:
+            logging.error(str(generic_err))
--- splitnjoin/snj_benchmark.py
+++ splitnjoin/snj_benchmark.py
@@ -19,61 +19,59 @@ def generate_file_md5(filename, blocksize):
 fsplitter = snj.FileProcessor()
 fjoiner = snj.FileProcessor()
 digests = list()
-
 blocksize = 250000000
-
 p_size = 250
 from_file = "fake_data.bin"
 to_dir = "test"
-
 read_size = 250
 from_dir = "test"
 to_file = "joined_fake_data.bin"
-
 snj_ver = pkg_resources.get_distribution("splitnjoin").version
 print("[!]'splitnjoin' ver.", snj_ver, "Benchmark&Test tool\n")
 print('[+] Generating fake binary file of 1 GB...')
 print('[+] Please, wait...')
 start = timer()
-with open(os.path.abspath(from_file), 'wb') as fout:
-    fout.write(os.urandom(1073741824)) 
-end = timer()
-print("[+]", from_file, "written.")
-print('[+] Writing time: ', end - start)
-
-print("")
-absfrom, absto = map(os.path.abspath, [from_file, to_dir])
-print('[+] Splitting', absfrom, 'to', absto, 'by', p_size, 'mb...')
-print('[+] Please, wait...')
-start = timer()
-fsplitter.split_file(from_file, p_size, to_dir)
-end = timer()
-print('[+] Splitting time: ', end - start)
-
-print("")
-absfrom, absto = map(os.path.abspath, [from_dir, to_file])
-print('[+] Joining', absfrom, 'to', absto, 'by', read_size, 'mb...')
-print("[+] Please, wait...")
-start = timer()
-fjoiner.join_file(from_dir, read_size, to_file)
-end = timer()
-print("[+] Joining time: ", end - start)
-
-print("")
-print("[+] Calculating md5 hash for both files...")
-print("[+] Please wait...")
-start = timer()
-digests.append(generate_file_md5(from_file, blocksize))
-digests.append(generate_file_md5(to_file, blocksize))
-print("[+] md5:", digests[0], "for", from_file)
-print("[+] md5:", digests[1], "for", to_file)
-end = timer()
-print("[+] Hashing time: ", end - start)
-if digests[0]==digests[1]:
-    print("\n[+] Integrity Check OK, the files are identical.")
-else:
-    print("\n[!] Error: Check FAILED! Files are diffent (prob. corruption/losses)")
-
+try:
+    with open(os.path.abspath(from_file), 'wb') as fout:
+        fout.write(os.urandom(1073741824)) 
+    end = timer()
+    print("[+]", from_file, "written.")
+    print('[+] Writing time: ', end - start)
+    print("")
+    absfrom, absto = map(os.path.abspath, [from_file, to_dir])
+    print('[+] Splitting', absfrom, 'to', absto, 'by', p_size, 'mb...')
+    print('[+] Please, wait...')
+    start = timer()
+    fsplitter.split_file(from_file, p_size, to_dir)
+    end = timer()
+    print('[+] Splitting time: ', end - start)
+    print("")
+    absfrom, absto = map(os.path.abspath, [from_dir, to_file])
+    print('[+] Joining', absfrom, 'to', absto, 'by', read_size, 'mb...')
+    print("[+] Please, wait...")
+    start = timer()
+    fjoiner.join_file(from_dir, read_size, to_file)
+    end = timer()
+    print("[+] Joining time: ", end - start)
+    print("")
+    print("[+] Calculating md5 hash for both files...")
+    print("[+] Please wait...")
+    start = timer()
+    digests.append(generate_file_md5(from_file, blocksize))
+    digests.append(generate_file_md5(to_file, blocksize))
+    print("[+] md5:", digests[0], "for", from_file)
+    print("[+] md5:", digests[1], "for", to_file)
+    end = timer()
+    print("[+] Hashing time: ", end - start)
+    if digests[0]==digests[1]:
+        print("\n[+] Integrity Check OK, the files are identical.")
+    else:
+        print("\n[!] Error: Check FAILED! Files are diffent (prob. corruption/losses)")
+except KeyboardInterrupt:
+    print("[!] Script stopped (Ctrl+C).")
+    os.remove(from_file)
+    shutil.rmtree(from_dir, ignore_errors=True)
+    sys.exit()    
 print("")
 print("[+] Removing test files...")
 for filename in [from_file, to_file]:

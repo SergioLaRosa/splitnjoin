014a1682f98c04a421ef973eb499f5e447f029df
Sergio La Rosa <sergio.larosa89@gmail.com>
2018-03-22 18:54:33 +0100

    ver_0.66

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@
 
 from setuptools import setup
 
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = ''
+
 setup(
     name='splitnjoin',
-    version='0.65',
+    version='0.66',
     description='Simple module for splitting binary files into multiple chunks and viceversa (from chunks to original file)',
+    long_description=long_description,
     url='https://github.com/SergioLaRosa/splitnjoin',
     author='Sergio La Rosa',
     author_email='sergio.larosa89@gmail.com',

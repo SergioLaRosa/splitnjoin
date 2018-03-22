3dfabc02e9587db77e09deb68da080b55c4bc8bc
Sergio La Rosa <sergio.larosa89@gmail.com>
2018-03-22 14:13:18 +0100

    ver_0.62

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# Part of "splitnjoin" package
+# https://github.com/SergioLaRosa/splitnjoin
+
 from setuptools import setup
 
 setup(
     name='splitnjoin',
-    version='0.60',
+    version='0.62',
     description='Simple module for splitting binary files into multiple chunks and viceversa (from chunks to original file)',
     url='https://github.com/SergioLaRosa/splitnjoin',
     author='Sergio La Rosa',
